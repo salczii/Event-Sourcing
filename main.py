2930a8d343ec80c5fa577bd88b29dd8cd194fd1c
salczii <kacper.salek32@gmail.com>
2023-08-01 22:03:46 +0200

    add pour_water, pour_out_water and transfer_water methods to the Tank class

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Tank:
+    tanks = []
+
+    def __init__(self, name, volume, capacity):
+        self.name = name
+        self.capacity = capacity
+        self.water_volume = 0
+        Tank.tanks.append(self)
+
+    def pour_water(self, volume):
+        if self.water_volume + volume > self.capacity:
+            print(f"you are not allowed to add more water than {self.capacity - self.water_volume}")
+        else:
+            self.water_volume += volume
+
+    def pour_out_water(self, volume):
+        if volume <= self.water_volume:
+            self.water_volume -= volume
+        else:
+            print(f"Cannot pour out more than {self.water_volume} water.")
+
+    def transfer_water(self, from_tank, volume):
+        if self.water_volume + volume <= self.capacity:
+            from_tank.water_volume -= volume
+            self.water_volume += volume
+        else:
+            print("Transfer is not possible.")
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    tank = Tank('tank', 100)
